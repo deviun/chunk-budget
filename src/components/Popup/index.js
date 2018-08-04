e0d4758ea215850a4d5ed549ab8a7b40bb2e9e7f
Anton Danilov <a.deviun@gmail.com>
2018-08-04 13:56:02 +0300

    write popup component

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -4,22 +4,49 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
+// actions
+import closePopup from '../../actions/closePopup';
+
 class Popup extends Component {
+  constructor (props) {
+    super(props);
+
+    this.closePopup = (event) => {
+      if (event.currentTarget == event.target) { 
+        props.closePopup(props.id);
+      }
+    };
+  }
   render() {
     return (
-      true
+      <div className="popup-box" id={this.props.id} onClick={this.closePopup}>
+        <div className="box">
+          <div className="popup-name">{this.props.name}</div>
+          <div className="popup-content">
+            {this.props.children}
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
 Popup.propTypes = {
-  
+  name: PropTypes.string.isRequired,
+  closePopup: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired
 };
 
-function mapStateToProps(state) {
-  return {
+// function mapStateToProps(state) {
+//   return {
     
-  };
+//   };
+// }
+
+function mapDispatchToProps (dispatch) {
+  return {
+    closePopup: (id) => dispatch(closePopup(id))
+  }
 }
 
-export default connect(mapStateToProps, null)(Popup);
+export default connect(null, mapDispatchToProps)(Popup);
