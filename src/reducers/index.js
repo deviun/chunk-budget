2d68df4948a6721c938962536550238173ef3e7e
Anton Danilov <a.deviun@gmail.com>
2018-08-04 12:34:22 +0300

    add popup reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import welcome from './welcome';
 import expenseTable from './expenseTable';
+import popups from './popups';
 
 export default combineReducers({
   welcome,
-  expenseTable
+  expenseTable,
+  popups
 });
