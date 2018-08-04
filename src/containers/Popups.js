9803ad502e35384d48fefced5992d5cd237115dd
Anton Danilov <a.deviun@gmail.com>
2018-08-04 13:56:41 +0300

    write popups manager

--- src/containers/Popups.js
+++ src/containers/Popups.js
@@ -2,15 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+// components
+import Popup from '../components/Popup';
+
+// body of popups
+import PointEditor from '../components/PointEditor';
+
+// producers
+
+function CreatePoint () {
+  return (
+    <Popup
+      name="Create a point"
+      id="addPoint"
+      key="addPoint"
+    >
+      <PointEditor />
+    </Popup>
+  );
+}
+
 /**
  * @class Popups
  * @description Manager popup windows
  */
 class Popups extends Component {
+  static get reference () {
+    return {
+      addPoint: CreatePoint
+    }
+  }
+
   render() {
-    return (
-      <div>Open popups: {JSON.stringify(this.props.open)}</div>
-    );
+    return this.props.open.map((popup) => {
+      const Component = Popups.reference[popup.id];
+
+      return <Component data={popup.data} />;
+    });
   }
 }
 
