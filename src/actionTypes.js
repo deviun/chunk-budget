719f09e4f9ab4cb706eb4e5939c3956ba44b00a0
Anton Danilov <a.deviun@gmail.com>
2018-08-09 20:17:41 +0300

    add mod action name for point editor

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -5,3 +5,4 @@ export const TURN_EDIT_CELL = 'TURN_EDIT_CELL';
 export const SAVE_CELL_VALUE = 'SAVE_CELL_VALUE';
 export const OPEN_POPUP = 'OPEN_POPUP';
 export const CLOSE_POPUP = 'CLOSE_POPUP';
+export const MOD_POINT_EDITOR = 'MOD_POINT_EDITOR';
