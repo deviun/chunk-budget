3c1feba3aa094e37e4f205534261aac699978e86
Anton Danilov <a.deviun@gmail.com>
2018-08-09 20:18:10 +0300

    create reducer for point editor

--- src/components/PointEditor/index.js
+++ src/components/PointEditor/index.js
@@ -8,8 +8,8 @@ class PointEditor extends Component {
   render() {
     return (
       <div className="point-editor">
-        <input type="text" className="name" placeholder="Point name" />
-        <input type="number" className="amount-percent" placeholder="0%" min="1" max="100" />
+        <input type="text" value={this.props.form.name} className="name" placeholder="Point name" />
+        <input type="number" value={this.props.form.amountPercent} className="amount-percent" placeholder="0%" min="1" max="100" />
         <div className="left-percents">Left 5% for points</div>
         <button>Save</button>
       </div>
@@ -18,13 +18,16 @@ class PointEditor extends Component {
 }
 
 PointEditor.propTypes = {
-  
+  form: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    amountPercent: PropTypes.number.isRequired
+  }).isRequired
 };
 
-// function mapStateToProps(state) {
-//   return {
+function mapStateToProps(state) {
+  return {
+    form: state.pointEditor
+  };
+}
 
-//   };
-// }
-
-export default connect(null, null)(PointEditor);
+export default connect(mapStateToProps, null)(PointEditor);
